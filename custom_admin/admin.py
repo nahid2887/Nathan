f08ebdedabe82dd18f842e40b4da64e58d8e23ee
nahid2887 <nahid2887@gmail.com>
2026-07-07 10:37:19 +0600

    admin: register SubscriptionPlan model in Django Admin

--- custom_admin/admin.py
+++ custom_admin/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from .models import SubscriptionPlan
 
-# Register your models here.
+@admin.register(SubscriptionPlan)
+class SubscriptionPlanAdmin(admin.ModelAdmin):
+    list_display = (
+        'id', 
+        'name', 
+        'price', 
+        'billing_cycle', 
+        'discount_offer', 
+        'created_at', 
+        'updated_at'
+    )
+    list_filter = ('billing_cycle', 'created_at')
+    search_fields = ('name',)
+    ordering = ('-created_at',)
