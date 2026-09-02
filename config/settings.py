3e9821e027bf2ccfaecd6ea9cc3097491d0b6b66
nahid2887 <nahid2887@gmail.com>
2026-09-02 11:39:16 +0600

    feat: initialize Django project configuration with environment variable support, database routing, and service integrations

--- config/settings.py
+++ config/settings.py
@@ -35,18 +35,21 @@ if allowed_hosts_env:
 else:
     ALLOWED_HOSTS = ['hoodlink.duckdns.org', '66.226.146.115', '10.10.13.27', '127.0.0.1', 'localhost', '*']
 
+DEFAULT_TRUSTED_ORIGINS = [
+    'https://hoodlink.duckdns.org',
+    'http://hoodlink.duckdns.org',
+    'http://66.226.146.115',
+    'https://66.226.146.115',
+    'http://localhost',
+    'http://127.0.0.1',
+]
 csrf_trusted_origins_env = os.environ.get('CSRF_TRUSTED_ORIGINS', '')
 if csrf_trusted_origins_env:
-    CSRF_TRUSTED_ORIGINS = [origin.strip() for origin in csrf_trusted_origins_env.split(',') if origin.strip()]
+    env_origins = [origin.strip() for origin in csrf_trusted_origins_env.split(',') if origin.strip()]
+    CSRF_TRUSTED_ORIGINS = list(set(DEFAULT_TRUSTED_ORIGINS + env_origins))
 else:
-    CSRF_TRUSTED_ORIGINS = [
-        'https://hoodlink.duckdns.org',
-        'http://hoodlink.duckdns.org',
-        'http://66.226.146.115',
-        'https://66.226.146.115',
-        'http://localhost',
-        'http://127.0.0.1',
-    ]
+    CSRF_TRUSTED_ORIGINS = DEFAULT_TRUSTED_ORIGINS
+
 
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
